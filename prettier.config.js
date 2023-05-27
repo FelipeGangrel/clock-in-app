89215b9b56ac3ca35a35eca57e1a09c4ae089cb1
Felipe Gangrel <felipefrog@gmail.com>
2023-05-27 13:49:20 -0300

    Add prettier and vscode config

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindFunctions: ['cva', 'clsx', 'tw', 'twMerge'],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+}
